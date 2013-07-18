a2d79973b4c248befcf29cc1abea8ff46a44ff9c
Andre Lynum <and-ly@online.no>
2013-07-18 21:28:46 +0200

    Processor writing HunPos/TnT format files.

---
+++ scripts/hunpos_processor.rb
@@ -0,0 +1,128 @@
+require_relative 'base_processor'
+
+class HunposProcessor < BaseProcessor
+
+  attr_reader :num_folds, :descr
+
+  def initialize(opts={})
+    super(opts[:processor] || nil)
+
+    @base_name = opts[:base_name] || nil
+    @descr = opts[:descr] || nil
+    @num_folds = opts[:num_folds] || 1
+
+    if @base_name and @descr
+      raise ArgumentError
+    end
+  end
+
+  def process(sent)
+    if not @descr
+      @descr = create_descr(@base_name)
+    end
+
+    words = sent[:words]
+    fold = get_fold sent
+    len = words.count
+
+    words.each_with_index do |word, i|
+      form = word[:form]
+      pos = word[:pos]
+
+      if word.has_key? :feat and word[:feat] != '_'
+        pos = pos + '_' + word[:feat].split('|').join('_')
+      end
+
+      if fold
+        @descr.each_with_index do |fold_descr, i|
+          if i == fold
+            write_test_file fold_descr[:pred_file], form
+            write_word fold_descr[:true_file], form, pos
+          else
+            write_word fold_descr[:in_file], form, pos
+          end
+        end
+      else
+        write_word @descr[0][:in_file], form, pos
+      end
+    end
+
+    if fold
+      @descr.each_with_index do |fold_descr, i|
+        if i == fold
+          fold_descr[:pred_file].puts
+          fold_descr[:true_file].puts
+        else
+          fold_descr[:in_file].puts
+        end
+      end
+    else
+      @descr[0][:in_file].puts
+    end
+
+    return sent
+  end
+
+  def post_process
+    @descr = @descr[0] unless has_folds?
+  end
+
+  def get_fold(sent)
+    if has_folds?
+      return sent[:fold]
+    else
+      return nil
+    end
+  end
+
+  def write_word(file, form, pos)
+    file.puts "#{form}\t#{pos}"
+  end
+
+  def write_test_file(file, form)
+    file.puts form
+  end
+
+  def create_descr(base_name=nil)
+    if base_name
+      if has_folds?
+        @descr = (0...num_folds).collect do |i|
+          { in_file: File.new("#{base_name}_#{i}_in", 'w'),
+            pred_file: File.new("#{base_name}_#{i}_pred", 'w'),
+            true_file: File.new("#{base_name}_#{i}_true", 'w') }
+        end
+      else
+        @descr = [{ in_file: File.new("#{base_name}_in", 'w') }]
+      end
+    else
+      if has_folds?
+        @descr = (0...num_folds).collect do |i|
+          { in_file: StringIO.new,
+            pred_file: StringIO.new,
+            true_file: StringIO.new }
+        end
+      else
+        # wraps single descr in array
+        @descr = [{ in_file: StringIO.new }]
+      end
+    end
+
+    return @descr
+  end
+
+  def num_folds=(n)
+    if not @descr
+      @num_folds = n
+
+      if @processor
+        @processor.num_folds = n
+      end
+    else
+      raise RuntimeError
+    end
+  end
+
+  def has_folds?
+    return @num_folds > 1
+  end
+end
---
+++ scripts/test/hunpos_processor_test.rb
@@ -0,0 +1,53 @@
+require 'test/unit'
+
+require_relative '../hunpos_processor'
+require_relative 'data_repository'
+require_relative '../array_source'
+
+class HunposProcessorTest < Test::Unit::TestCase
+  def test_hunpos_processor
+    writer = HunposProcessor.new
+    src = ArraySource.new DataRepository.sample4, writer
+
+    result = src.to_a
+    descr = writer.descr
+
+    assert_not_nil descr
+    assert_not_nil descr[:in_file]
+    exp_str = "Verdensarv\tsubst_prop\n" +
+        ".\t<punkt>\n\n" +
+        "Reise\tsubst_appell_fem_ub_ent\n" +
+        "til\tprep\n" +
+        "Kina\tsubst_prop\n" +
+        ":\t<kolon>\n\n"
+    assert_equal exp_str, descr[:in_file].string
+  end
+
+  def test_hunpos_with_folds
+    writer = HunposProcessor.new
+    fold_gen = FoldProcessor.new(processor: writer, num_folds: 2)
+
+    assert_equal 2, writer.num_folds
+    assert writer.has_folds?
+    writer.create_descr
+    assert_kind_of Enumerable, writer.descr
+    assert_equal 2, writer.descr.size
+    assert_equal 2, writer.descr.size
+
+    src = ArraySource.new(DataRepository.sample2, fold_gen)
+    src.process_all
+
+    assert_raise(RuntimeError) { fold_gen.num_folds = 3 }
+
+    descr = writer.descr
+
+    assert_equal "ba\tsubst_ent\n.\tclb\n\n", descr[1][:in_file].string
+    assert_equal "gneh\tverb_pres\n.\tclb\n\n", descr[0][:in_file].string
+    assert_equal "ba\tsubst_ent\n.\tclb\n\n", descr[0][:true_file].string
+    assert_equal "gneh\tverb_pres\n.\tclb\n\n", descr[1][:true_file].string
+
+    # TODO again different order shouldn't fail tests
+    assert_equal "ba\n.\n\n", descr[0][:pred_file].string
+    assert_equal "gneh\n.\n\n", descr[1][:pred_file].string
+      end
+end
