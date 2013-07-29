1d1cc8a1091cc7eaaec83a1de232d08c69d5e64b
Andre Lynum <and-ly@online.no>
2013-07-29 11:07:55 +0200

    Piped input/output to shell processes in Utilities::run_shell_command().

--- scripts/test/utilities_test.rb
+++ scripts/test/utilities_test.rb
@@ -1,10 +1,20 @@
 require 'test/unit'
 
+require 'stringio'
+
 require_relative '../utilities'
 
 class UtilitiesTest < Test::Unit::TestCase
   def test_run_shell_cmd
     # pretend we're in a unixy place
-    assert_equal 0, Utilities.run_shell_command('ls')
+    status = Utilities.run_shell_command('ls')
+    assert_equal 0, status.exitstatus
+
+    in_str = "ba\nfoo\nbork\nknark\n"
+    in_file = StringIO.new(in_str)
+    out_file = StringIO.new
+    status = Utilities.run_shell_command('cat', stdin_file=in_file, stdout_file=out_file)
+    assert_equal 0, status.exitstatus
+    assert_equal in_file.string, out_file.string
   end
 end
--- scripts/utilities.rb
+++ scripts/utilities.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'io/wait'
 
 class Utilities
   def self.multiple_file_open(filenames, perm, &block)
@@ -14,11 +15,59 @@ class Utilities
     return Marshal.load(Marshal.dump(obj))
   end
 
-  def self.run_shell_command(cmd)
-    oe, s = Open3.capture2e(cmd)
+  # Runs an external shell command.
+  #
+  # @note Stderr output is written to STDERR. Stdout output is written to STDOUT if no stdout_file
+  #   argument is given.
+  #
+  # @param cmd [String] The shell command string. Should not include pipes.
+  # @param stdin_file [IO, NilClass] IO instance to read input to the shell process from.
+  # @param stdout_file [IO, NilClass] IO instance to write shell process output to.
+  # @return [Process::Status] Shell command exit status.
+  def self.run_shell_command(cmd, stdin_file=nil, stdout_file=nil)
+    if stdin_file
+      stdin, stdout, stderr, thr = Open3.popen3 cmd
 
-    puts oe
-    puts s
+      oe = ""
+      err = ""
+
+      # read and write stdin/stdout/stderr to avoid deadlocking on processes that blocks on writing.
+      # e.g. HunPos
+      stdin_file.each_line do |line|
+        stdin.puts line
+
+        while stdout.ready?
+          oe += stdout.readline
+        end
+
+        while stderr.ready?
+          err += stderr.readline
+        end
+      end
+
+      stdin.close
+
+      # get the rest of the output
+      oe += stdout.read
+      stdout.close
+
+      err += stderr.read
+      stderr.close
+
+      # wait and get get Process::Status
+      s = thr.value
+    else
+      out, err, s = Open3.capture3(cmd)
+    end
+
+    # echo errors on STDERR
+    STDERR.puts err
+
+    if stdout_file
+      stdout_file.write(oe)
+    else
+      puts out
+    end
 
     return s
   end
