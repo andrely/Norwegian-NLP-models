44213804cfb0ebfc7728c0d725ff94d3791c2103
Andre Lynum <and-ly@online.no>
2013-08-19 18:01:00 +0200

    Refactored POSBuilderProcessor into cleaning up and extracting bare POS.

--- scripts/pos_builder_processor.rb
+++ scripts/pos_builder_processor.rb
@@ -1,20 +1,112 @@
+# encoding: utf-8
+
 require_relative 'base_processor'
 
+##
+# Processor for sources read from SB Gull or OBT development corpora.
+# Cleans up POS and feature information on words:
+# - Removes extranous clb, contok and syntactic features
+# - collects bare POS information in :pos field
+# - Puts remaining information in :feat field, sorted alphabetically
+
 class POSBuilderProcessor < BaseProcessor
+  FEAT_SEP = '#'
+  CLEAN_TAG_REGEX = Regexp.compile('((i|pa|tr|pr|r|rl|a|d|n)\d+(\/til)?)')
+
   def process(sent)
     sent[:words].each do |word|
-      if word[:pos].match /\s+/
-        word[:pos] = word[:pos].split().join('_')
+      # first join together the pos and any features
+      if word.has_key? :feat and word[:feat] != '_'
+        pos = word[:pos] + ' ' + (word[:feat].split('|')).join(' ')
+      else
+        pos = word[:pos]
       end
 
-      if word.has_key? :feat and word[:feat] != '_'
-        pos = word[:pos]
-        word[:pos_bare] = pos
-        pos = pos + '_' + word[:feat].split('|').join('_')
-        word[:pos] = pos
-      end
+      # clean up the pos like in OBT-Stat
+      pos = clean_out_tag(pos).downcase
+
+      # split up back into pos/features list
+      parts = pos.split(FEAT_SEP)
+      pos = parts.shift
+      parts.uniq!
+
+      # remove unwanted features
+      parts.delete_if { |p| ['<contok>', '<ikke-clb>', 'clb'].include?(p) }
+      parts.sort!
+
+      # construct full pos
+      pos, parts = get_base_tag(pos, parts)
+
+      word[:pos] = pos
+      word[:feat] = parts
+
+      # TODO process the forms in a dedicated processor
+      word[:form] = "\"" if ["«", "»"].include?(word[:form])
+
+      # TODO lemmas should heve the case properly restored
+      word[:lemma] = word[:lemma].delete('*') if word[:lemma].match('\w+')
     end
 
-    return sent
+    sent
   end
+
+  ##
+  # @private
+  # Copied from OBT-Stat
+  def clean_out_tag(pos)
+    # remove unnecessary info from the tag field for "joined words". These words
+    # uniquely have a @ in their tag, with the tag being the token in front of this.
+    #
+    # ie. "prep+subst prep @adv" is turned into "prep" from the middle field
+    if pos.match('@')
+      pos = pos.gsub(/^[\w\+]+\s(\w+)\s@.+$/, '\1')
+    end
+
+    # we treat clb marked punctuation the same as unmarked
+    if pos.match(/^clb /)
+      pos = pos.gsub(/^clb (.*)$/, '\1')
+    end
+
+    pos.gsub(CLEAN_TAG_REGEX, '').strip.gsub(/\s+/, FEAT_SEP)
+  end
+
+  ##
+  # @private
+  # Extracts the bare pos from other features
+  def get_base_tag(pos, parts)
+    if pos == 'det' and parts.include?('dem')
+      pos = 'det_dem'
+      parts.delete('dem')
+    elsif pos == 'det' and parts.include?('kvant')
+      pos = 'det_kvant'
+      parts.delete('kvant')
+    elsif pos == 'det' and parts.include?('poss')
+      pos = 'det_poss'
+      parts.delete('poss')
+    elsif pos == 'pron' and parts.include?('pers')
+      pos = 'pron_pers'
+      parts.delete('pers')
+    elsif pos == 'subst' and parts.include?('appell')
+      pos = 'subst_appell'
+      parts.delete('appell')
+    elsif pos == 'verb' and parts.include?('imp')
+      pos = 'verb_imp'
+      parts.delete('imp')
+    elsif pos == 'verb' and parts.include?('inf')
+      pos = 'verb_inf'
+      parts.delete('inf')
+    elsif pos == 'verb' and parts.include?('pret')
+      pos = 'verb_pret'
+      parts.delete('pret')
+    elsif pos == 'verb' and parts.include?('pres')
+      pos = 'verb_pres'
+      parts.delete('pres')
+    elsif pos == 'verb' and parts.include?('perf-part')
+      pos = 'verb_perf-part'
+      parts.delete('perf-part')
+    end
+
+    return pos, parts
+  end
+
 end
--- scripts/test/pos_builder_processor_test.rb
+++ scripts/test/pos_builder_processor_test.rb
@@ -8,7 +8,7 @@ require_relative 'data_repository'
 
 class POSBuilderProcessorTest < Test::Unit::TestCase
   def test_pos_builder_processor
-    src = ConllSource.new(StringIO.new(DataRepository.sample_conll),
+    src = ConllSource.new(StringIO.new(DataRepository.sample_conll_1),
                           processor: POSBuilderProcessor.new)
 
     result = src.to_a
@@ -18,43 +18,43 @@ class POSBuilderProcessorTest < Test::Unit::TestCase
     sent = result[0]
     words = sent[:words]
     assert_not_nil words
-    assert_equal DataRepository.sample_conll_sent_sizes[0], words.size
-    assert_equal 'det_kvant_fl', words[0][:pos]
-    assert_equal 'det', words[0][:pos_bare]
-    assert_equal 'subst_mask_appell_ub_fl', words[1][:pos]
-    assert_equal 'subst', words[1][:pos_bare]
+    assert_equal DataRepository.sample_conll_1_sent_sizes[0], words.size
+    assert_equal 'det_kvant', words[0][:pos]
+    assert_equal ['fl'], words[0][:feat]
+    assert_equal 'subst_appell', words[1][:pos]
+    assert_equal ['fl', 'mask', 'ub'], words[1][:feat]
     assert_equal 'prep', words[2][:pos]
-    assert_nil words[2][:pos_bare]
-    assert_equal 'subst_mask_appell_eint_bu', words[3][:pos]
-    assert_equal 'subst', words[3][:pos_bare]
-    assert_equal '<komma>_<ikke-clb>', words[4][:pos]
-    assert_equal '<komma>', words[4][:pos_bare]
+    assert_equal [], words[2][:feat]
+    assert_equal 'subst_appell', words[3][:pos]
+    assert_equal ['bu', 'eint', 'mask'], words[3][:feat]
+    assert_equal '<komma>', words[4][:pos]
+    assert_equal [], words[4][:feat]
     assert_equal 'prep', words[5][:pos]
-    assert_nil words[5][:pos_bare]
-    assert_equal 'subst_mask_prop', words[6][:pos]
-    assert_equal 'subst', words[6][:pos_bare]
-    assert_equal 'konj_<ikkje-clb>', words[7][:pos]
-    assert_equal 'konj', words[7][:pos_bare]
+    assert_equal [], words[5][:feat]
+    assert_equal 'subst', words[6][:pos]
+    assert_equal ['mask', 'prop'], words[6][:feat]
+    assert_equal 'konj', words[7][:pos]
+    assert_equal [], words[7][:feat]
     assert_equal '<anf>', words[8][:pos]
-    assert_nil words[8][:pos_bare]
-    assert_equal 'subst_appell_ub_fl_unorm', words[9][:pos]
-    assert_equal 'subst', words[9][:pos_bare]
+    assert_equal [], words[8][:feat]
+    assert_equal 'subst_appell', words[9][:pos]
+    assert_equal ['fl', 'ub', 'unorm'], words[9][:feat]
     assert_equal '<anf>', words[10][:pos]
-    assert_nil words[10][:pos_bare]
-    assert_equal 'clb_<overskrift>', words[11][:pos]
-    assert_equal 'clb', words[11][:pos_bare]
+    assert_equal [], words[10][:feat]
+    assert_equal '<overskrift>', words[11][:pos]
+    assert_equal [], words[11][:feat]
 
     sent = result[1]
     words = sent[:words]
     assert_not_nil words
-    assert_equal DataRepository.sample_conll_sent_sizes[1], words.size
-    assert_equal 'pron_pers_1_eint_hum_nom', words[0][:pos]
-    assert_equal 'pron', words[0][:pos_bare]
-    assert_equal 'verb_pret_<aux1/perf_part>', words[1][:pos]
-    assert_equal 'verb', words[1][:pos_bare]
+    assert_equal DataRepository.sample_conll_1_sent_sizes[1], words.size
+    assert_equal 'pron_pers', words[0][:pos]
+    assert_equal ['1', 'eint', 'hum', 'nom'], words[0][:feat]
+    assert_equal 'verb_pret', words[1][:pos]
+    assert_equal ['<aux1/perf_part>'], words[1][:feat]
     assert_equal 'prep', words[2][:pos]
-    assert_nil words[2][:pos_bare]
-    assert_equal 'subst_mask_appell_ub_eint', words[3][:pos]
-    assert_equal 'subst', words[3][:pos_bare]
+    assert_equal [], words[2][:feat]
+    assert_equal 'subst_appell', words[3][:pos]
+    assert_equal ['eint', 'mask', 'ub'], words[3][:feat]
   end
 end
