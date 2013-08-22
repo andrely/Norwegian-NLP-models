2824843bd02e373e9719b158bf41d4767b107ace
Andre Lynum <and-ly@online.no>
2013-08-22 11:31:07 +0200

    Added treetagger full model.

--- scripts/create_models.rb
+++ scripts/create_models.rb
@@ -8,6 +8,8 @@ require_relative 'conll_source'
 require_relative 'concatenated_source'
 require_relative 'utilities'
 require_relative 'hunpos_model'
+require_relative 'tree_tagger_processor'
+require_relative 'tree_tagger_model'
 
 BASE_PATH = "../models-#{Date.today.to_s}"
 
@@ -32,12 +34,38 @@ def create_hunpos_files(args)
   end
 end
 
+def create_tt_files(args)
+  writer = TreeTaggerProcessor.new(args)
+  stats = FrequencyStatisticsProcessor.new(processor: writer)
+  pos = POSBuilderProcessor.new(processor: stats)
+
+  Utilities.multiple_file_open([GULL_PATH, OBT_PATH], 'r') do |files|
+    gull_file, obt_file = files
+
+    gull_src = ConllSource.new(gull_file)
+    obt_src = ConllSource.new(obt_file, columns: [:form, :lemma, :pos])
+    src = ConcatenatedSource.new([gull_src, obt_src], processor: pos)
+
+    src.process_all
+
+    return src.pipeline_artifacts
+  end
+end
+
 def create_hunpos_model(artifact)
-  model = HunposModel.new(artifact: artifact)
-  model.train
+  HunposModel.new(artifact: artifact)
+end
+
+def create_tt_model(artifact)
+  TreeTaggerModel.new(artifact: artifact)
 end
 
 hunpos_path = File.join(BASE_PATH, 'hunpos')
 FileUtils.mkpath(hunpos_path) unless File.exists?(hunpos_path)
 artifacts = create_hunpos_files({ base_name: File.join(hunpos_path, "hunpos")})
 create_hunpos_model(artifacts[0])
+
+tt_path = File.join(BASE_PATH, 'treetagger')
+FileUtils.mkpath(tt_path) unless File.exists?(tt_path)
+artifacts = create_tt_files({ base_name: File.join(tt_path, 'treetagger') })
+create_tt_model(artifacts[0])
